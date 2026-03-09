30b6662a190f1a56527cb0e87eb212636a289aa6
garidana-collab <garidana@gmail.com>
2026-03-10 02:10:45 +0900

    35
    Shape Classifier in progress

---
+++ ItemSystem/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Item
+{
+    public string Name;
+    public string Rarity;
+}
+
+class Weapon : Item
+{
+    public int Damage;
+    public string WeaponType;
+}
+
+class Armor : Item
+{
+    public int Defense;
+    public string Armortype;
+}
+
+class Consumable : Item
+{
+    public string Effect;
+    public int Duration;
+}
